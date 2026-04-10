97d9d5ac95bbb1daaf4b84361bcc55e30f7fcd18
Daniel Jacobs <daniel.jacobs@is4s.com>
2026-04-10 14:41:26 -0400

    use same llh0 for all channels

--- analysis-lcm/src/analysis/lcm/log_readers/PosLogReader.py
+++ analysis-lcm/src/analysis/lcm/log_readers/PosLogReader.py
@@ -8,6 +8,8 @@ from .LogReader import LogReader
 
 
 class PosLogReader(LogReader[PosData]):
+    llh0: np.ndarray | None
+
     def __init__(
         self,
         logfile: str,
@@ -20,11 +22,20 @@ class PosLogReader(LogReader[PosData]):
         self.log_data.data[self.log_data.truth_channel] = self.new_data(
             self.log_data.truth_channel
         )
+        self.llh0 = None
 
     def postprocess(self):
+        if self.log_data.truth_channel is not None:
+            truth_data = self.log_data.data[self.log_data.truth_channel]
+            if len(truth_data.llh) > 0:
+                self.llh0 = truth_data.llh[0]
+
         for data in self.log_data.data.values():
             # Convert tov to relative time
             data.time = np.array([to_seconds(t - self.log_data.t0) for t in data.time])
             data.llh = np.array(data.llh)
             data.sig = np.array(data.sig)
-            data.ned = llh_to_ned(data.llh)
+
+            if self.llh0 is None:
+                self.llh0 = data.llh[0]
+            data.ned = llh_to_ned(data.llh, self.llh0)
